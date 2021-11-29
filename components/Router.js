414ce8bfe35600f2f1f023fd8d0d42e8481bfbb4
reasley <33018600+reasley-com@users.noreply.github.com>
2021-11-29 15:49:46 +0900

    Create Router.js

---
+++ components/Router.js
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import {HashRouter as Router, Route, Switch } from "react-router-dom"
+
+const AppRouter = () => {
+  Const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <Router>
+      <Switch>
+        <Router />
+      </Switch
+    </Router>
+  )
+}
